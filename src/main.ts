932f3b9b491043d07ce62c4a9e51e1136de3fbdb
tvalentijn <tjerk.valentijn@visma.com>
2024-10-15 15:21:56 +0200

    fix some typing bugs and improved formatting

--- src/main.ts
+++ src/main.ts
@@ -23,9 +23,9 @@ type CustomError = {
 
 /* ------------------------- test data --------------------------  */
 
-const firstAnimal: Right<Animal> = ({ skin: "fur", legs: 4 });
+const firstAnimal: Animal = { skin: "fur", legs: 4 };
 
-const firstMachine: Right<Machine> = ({ weight: 100, color: "red" });
+const firstMachine: Machine = { weight: 100, color: "red" };
 
 /* ------------------------- functions --------------------------  */
 
@@ -47,8 +47,8 @@ const combineAnimalAndMachine = (animal: Animal) => (machine: Machine) => {
   return ({ ...animal, ...machine });
 }
 
-// Composing the two applicatives
-// NOTE this does not work!
+/* --------  Composing the two applicatives with 'ap' --------------- */
+
 /*
  The error TypeError: otherValue.extract(...) is not a function you're encountering is likely due to a
  misunderstanding about how ap works in this context. The issue stems from trying to apply a curried function
@@ -56,13 +56,15 @@ const combineAnimalAndMachine = (animal: Animal) => (machine: Machine) => {
  EitherAsync does not directly support the applicative ap pattern in the same way you would expect in Either or
  similar functional types. Specifically, the issue arises when trying to use ap for asynchronous
  computations like EitherAsync.
-*/
-// const combinedResult: EitherAsync<CustomError, Combined> =
-//   EitherAsync.liftEither(Right(combineAnimalAndMachine)) // Lifting the combining function
-//     .ap(getAnimalFromApi()) // Apply the first applicative (animal)
-//     .ap(getMachineFromApi()) // Apply the second applicative (machine)
+ So this won't work:
 
-// Composing the two applicatives using chain and map
+ const combinedResult: EitherAsync<CustomError, Combined> =
+   EitherAsync.liftEither(Right(combineAnimalAndMachine)) // Lifting the combining function
+     .ap(getAnimalFromApi()) // Apply the first applicative (animal)
+     .ap(getMachineFromApi()) // Apply the second applicative (machine)
+*/
+
+/* --------------------- Composing the two applicatives using 'chain' and 'map' ------------------- */
 // NOTE this DOES work!
 const combinedResult: EitherAsync<CustomError, Combined> =
   getAnimalFromApi().chain(
@@ -73,9 +75,10 @@ const combinedResult: EitherAsync<CustomError, Combined> =
 
 /* ---------------------------- runtime ---------------------------  */
 
+// await + run will start and wait for the async effect to complete and will return the Either inside the EitherAsync
 const animalAndMachineEither: Either<CustomError, Combined> = await combinedResult.run();
 
-// To run the flow using some pattern matching on the forked result
+// To get to the data in the forked result, we use som pattern matching on it
 animalAndMachineEither.caseOf({
   Left: (error: CustomError) => {
     console.error("An error occurred:", error.message);
